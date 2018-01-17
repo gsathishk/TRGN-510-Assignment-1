be1b4a729f1b0fa492758cefd999308f87b69bfc
SathishKumar Ganesan <sganesan@usc.edu>
2018-01-17 07:00:17 +0000

    Final Commit

--- myspammer.sh
+++ myspammer.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-echo 'Hi Dr. Craig! This is a TRGNG10 assignment 1 spam mail!! OMG this is so much fun!!' | mail -s 'trgn510 spam mail' g.sathish.k@gmail.com
+echo 'Hi Dr. Craig! This is  TRGNG10 assignment 1 spam mail!! OMG this is so much fun!!' | mail -s 'trgn510 spam mail' davidwcr@usc.edu
